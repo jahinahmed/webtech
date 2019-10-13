1d984752d2dab5f1539b0ebe1fc50cb7a16d6944
jahinahmed <55645515+jahinahmed@users.noreply.github.com>
2019-10-13 10:10:21 +0600

    Get text

---
+++ abc.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$username = $_POST["Username"];
+$password = $_POST["Password"];
+$DOB = $_POST["Date_of_birth"];
+$Add = $_POST["Comments"];
+$Dept = $_POST["Dept"];
+$Gender = $_POST["gender"];
+
+echo $username." ".$password." ".$DOB." ".$Add." ".$Dept." ".$Gender;
+
+?>
+
+
+
+
+
